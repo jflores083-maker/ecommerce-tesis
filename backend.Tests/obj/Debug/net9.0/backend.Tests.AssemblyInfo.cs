be4fe8e78af43b5c425fbf22a745a43a7fcaaa8c
jflores083-maker <jflores083@alumnos.iua.edu.ar>
2026-03-17 11:29:11 -0300

    correcion de merge

--- backend.Tests/obj/Debug/net9.0/backend.Tests.AssemblyInfo.cs
+++ backend.Tests/obj/Debug/net9.0/backend.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42a968641aa2db610bb5c99561a1b189459cd2fb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e2581e9daa1917e9ecc4ea1f20ce5789ac0647d")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
